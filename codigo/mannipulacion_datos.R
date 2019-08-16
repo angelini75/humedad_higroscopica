645c2576160d99e7d13d01efb62a7189a1a87e21
mangelini <mangelini@srv4.unlu.edu.ar>
2019-08-16 15:21:07 -0300

    Aprendizaje de R para ciencia de datos. Capitulo 3. Adaptacion a set de datos propios

--- codigo/mannipulacion_datos.R
+++ codigo/mannipulacion_datos.R
@@ -6,4 +6,14 @@ library(tidyverse)
 library(readxl)
 datos <- read_excel("datos/TABLA R b.xlsx")
 
+#### Guia R para ciencia de datos
+# Capitulo 3 #####
+##https://es.r4ds.hadley.nz/visualizacion-de-datos.html ###
 
+library(datos)
+millas <- millas
+ggplot(data = millas) +
+  geom_point(mapping = aes(x = motor, y = autopista))
+# graficar mis datos
+ggplot(data = datos) +
+  geom_point(mapping = aes(x = seca, y = hh))
