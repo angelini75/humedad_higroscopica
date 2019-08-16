102377d8e34d5d22345daf4e63047a10143b1b27
mangelini <mangelini@srv4.unlu.edu.ar>
2019-08-16 15:19:43 -0300

    importar tabla excell

--- codigo/mannipulacion_datos.R
+++ codigo/mannipulacion_datos.R
@@ -2,4 +2,8 @@ library(tidyverse)
 #install.packages("remotes")
 #remotes::install_github("cienciadedatos/datos")
 
+## Datos propios
+library(readxl)
+datos <- read_excel("datos/TABLA R b.xlsx")
+
 
